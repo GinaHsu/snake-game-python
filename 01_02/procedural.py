d76043db7e4401cff60bae07446d7b084a9b69f0
Gina Hsu <gina@ginahsu.com>
2023-01-26 20:43:12 -0800

    Create procedural.py

---
+++ 01_02/procedural.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+color("red")
+
+# movement
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+
+forward(100)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+
+done()
